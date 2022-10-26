b7f94ea1cc6db0bd85bad5fa83e89e7e54592c10
Fabio Penna <ffmpenna@gmail.com>
2022-10-26 14:46:40 -0300

    retocando o css

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import mealIcon from '../images/mealIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
 import '../App.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   const [redirectDrinks, setRedirectDrinks] = useState(false);
@@ -17,25 +17,27 @@ function Footer() {
   };
 
   return (
-    <footer className="footer" data-testid="footer">
-      <button
-        onClick={ redirectDrink }
-        type="button"
-        data-testid="drinks-bottom-btn"
-        src={ drinkIcon }
-      >
-        <img src={ drinkIcon } alt="drink-icon" />
-      </button>
-      <button
-        onClick={ redirectMeal }
-        type="button"
-        data-testid="meals-bottom-btn"
-        src={ mealIcon }
-      >
-        <img src={ mealIcon } alt="meal-icon" />
-      </button>
-      {redirectDrinks && <Redirect to="/drinks" />}
-      {redirectMeals && <Redirect to="/meals" />}
+    <footer className="footer-container" data-testid="footer">
+      <div className="footer-content">
+        <button
+          onClick={ redirectDrink }
+          type="button"
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+        >
+          <img src={ drinkIcon } alt="drink-icon" />
+        </button>
+        <button
+          onClick={ redirectMeal }
+          type="button"
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+        >
+          <img src={ mealIcon } alt="meal-icon" />
+        </button>
+        {redirectDrinks && <Redirect to="/drinks" />}
+        {redirectMeals && <Redirect to="/meals" />}
+      </div>
     </footer>
   );
 }

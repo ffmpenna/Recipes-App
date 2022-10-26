5c9453b0edbdc43f5fab60de78822b2b467f4bdf
wlisses <wlissesfernando285@gmail.com>
2022-10-26 12:55:52 -0300

    feitas novas alterações no CSS

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { screen } from '@testing-library/react';
+import renderWithProvider from './helper/renderWithProvider';
 import Footer from '../Pages/Footer';
 
 describe('testa o componente Footer', () => {

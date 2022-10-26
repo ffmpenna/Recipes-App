47b3f67574bc9d057014d6416fa67e95674b7343
Fabio Penna <ffmpenna@gmail.com>
2022-10-26 14:52:54 -0300

    consertando o footer escondendo o conteúdo da página

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -17,28 +17,31 @@ function Footer() {
   };
 
   return (
-    <footer className="footer-container" data-testid="footer">
-      <div className="footer-content">
-        <button
-          onClick={ redirectDrink }
-          type="button"
-          data-testid="drinks-bottom-btn"
-          src={ drinkIcon }
-        >
-          <img src={ drinkIcon } alt="drink-icon" />
-        </button>
-        <button
-          onClick={ redirectMeal }
-          type="button"
-          data-testid="meals-bottom-btn"
-          src={ mealIcon }
-        >
-          <img src={ mealIcon } alt="meal-icon" />
-        </button>
-        {redirectDrinks && <Redirect to="/drinks" />}
-        {redirectMeals && <Redirect to="/meals" />}
-      </div>
-    </footer>
+    <div>
+      <div className="clear" />
+      <footer className="footer-container" data-testid="footer">
+        <div className="footer-content">
+          <button
+            onClick={ redirectDrink }
+            type="button"
+            data-testid="drinks-bottom-btn"
+            src={ drinkIcon }
+          >
+            <img src={ drinkIcon } alt="drink-icon" />
+          </button>
+          <button
+            onClick={ redirectMeal }
+            type="button"
+            data-testid="meals-bottom-btn"
+            src={ mealIcon }
+          >
+            <img src={ mealIcon } alt="meal-icon" />
+          </button>
+          {redirectDrinks && <Redirect to="/drinks" />}
+          {redirectMeals && <Redirect to="/meals" />}
+        </div>
+      </footer>
+    </div>
   );
 }
 

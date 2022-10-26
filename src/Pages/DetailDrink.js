d71a747a89a627ee23a8dfa6a6b1754b696b243b
Fabio Penna <ffmpenna@gmail.com>
2022-10-26 19:53:38 -0300

    consertando página de detalhes

--- src/Pages/DetailDrink.js
+++ src/Pages/DetailDrink.js
@@ -1,31 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import MyContext from '../context/MyContext';
 // import PropTypes from 'prop-types'
 
 function DetailDrink(prop) {
-  const { drinkz } = useContext(MyContext);
+  const { fetchRecipes, recipes } = useContext(MyContext);
   const { match } = prop;
   const { params } = match;
   const { id } = params;
 
+  useEffect(() => {
+    async function fetchMyAPI() {
+      await fetchRecipes('drinks');
+    }
+    fetchMyAPI();
+  }, []);
+
   const renderFoods = (f) => {
-    const bebida = [...drinkz.filter((e) => e.idDrink === f)];
+    const bebida = [...recipes.drinks.filter((e) => e.idDrink === f)];
     return bebida;
   };
 
   const drink = renderFoods(id);
   return (
     <div>
-      DetailDrink
-      <div>
-        <h2>{drink[0].strDrink}</h2>
-        <img
-          height="325"
-          width="425"
-          src={ drink[0].strDrinkThumb }
-          alt={ drink[0].strDrink }
-        />
-      </div>
+      {drink[0] ? (
+        <div>
+          <h2>{drink[0].strDrink}</h2>
+          <img
+            height="325"
+            width="425"
+            src={ drink[0].strDrinkThumb }
+            alt={ drink[0].strDrink }
+          />
+        </div>
+      ) : (
+        <h1>Carregando...</h1>
+      )}
     </div>
   );
 }
--- src/Pages/DetailMeal.js
+++ src/Pages/DetailMeal.js
@@ -1,31 +1,45 @@
 // import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import MyContext from '../context/MyContext';
 
 function DetailMeal(meal) {
-  const { foods } = useContext(MyContext);
+  const { fetchRecipes, recipes } = useContext(MyContext);
   const { match } = meal;
   const { params } = match;
   const { id } = params;
 
+  useEffect(() => {
+    async function fetchMyAPI() {
+      await fetchRecipes('meals');
+    }
+    fetchMyAPI();
+  }, []);
+
   const renderFoods = (f) => {
-    const food = [...foods.filter((e) => e.idMeal === f)];
-    return food;
+    if (recipes.meals) {
+      const food = [...recipes.meals.filter((e) => e.idMeal === f)];
+
+      return food;
+    }
   };
 
   const prato = renderFoods(id);
+
   return (
     <div>
-      DetailMeal
-      <div>
-        <h2>{prato[0].strMeal}</h2>
-        <img
-          height="325"
-          width="425"
-          src={ prato[0].strMealThumb }
-          alt={ prato[0].strMeal }
-        />
-      </div>
+      {prato[0] ? (
+        <div>
+          <h2>{prato[0].strMeal}</h2>
+          <img
+            height="325"
+            width="425"
+            src={ prato[0].strMealThumb }
+            alt={ prato[0].strMeal }
+          />
+        </div>
+      ) : (
+        <h1>Carregando...</h1>
+      )}
     </div>
   );
 }
